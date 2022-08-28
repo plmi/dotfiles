78b3e30a6537a13f55dd2615ccc921fc1060100c
plmi <65455617+plmi@users.noreply.github.com>
2022-08-28 18:44:21 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,3 +14,19 @@ fi
 if [ ! -f ~/.gitconfig ] && [ -f "${DOTFILES}/.gitconfig" ]; then
   ln -s "${DOTFILES}/.gitconfig" "${HOME}/.gitconfig"
 fi
+
+if [ ! -f ~/.zshrc ] && [ -f "${DOTFILES}/.zshrc" ]; then
+  ln -s "${DOTFILES}/.zshrc" "${HOME}/.zshrc"
+fi
+
+if [ ! -f ~/.config/redshift.conf ] && [ -f "${DOTFILES}/.config/redshift.conf" ]; then
+  ln -s "${DOTFILES}/.config/redshift.conf" "${HOME}/.config/redshift.conf"
+fi
+
+if [ ! -f ~/.xsession ] && [ -f "${DOTFILES}/.xsession" ]; then
+  ln -s "${DOTFILES}/.xsession" "${HOME}/.xsession"
+fi
+
+if [ ! -f ~/.antigenrc ] && [ -f "${DOTFILES}/.antigenrc" ]; then
+  ln -s "${DOTFILES}/.antigenrc" "${HOME}/.antigenrc"
+fi
