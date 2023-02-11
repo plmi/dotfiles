179e4993c1d1f1dcf7ad6023c76e3d84e2db7083
plmi <65455617+plmi@users.noreply.github.com>
2023-02-11 10:52:34 +0100

    Add vpn.sh binary

---
+++ .local/bin/vpn.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Credits: siddicky
+
+if [[ $(ip addr | grep tun0) == '' ]]; then
+  echo "OpenVPN Not Active";
+else
+  ip addr show dev tun0 | grep "inet\b" | awk '{print $2}'| cut -d/ -f1;
+fi
