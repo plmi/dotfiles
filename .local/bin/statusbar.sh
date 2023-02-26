0745374b2610769dbc9f7a73745b10b7150fd796
plmi <65455617+plmi@users.noreply.github.com>
2023-02-26 12:01:32 +0100

    Update statusbar

--- .local/bin/statusbar.sh
+++ .local/bin/statusbar.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-export WEATHER_DEG=0
-
-ip() {
-  public="$(dig +short myip.opendns.com @resolver1.opendns.com)"
-  private="$(hostname -I | awk '{print $1}')"
+local_ip() {
+  #public="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+  #private="$(hostname -I | awk '{print $1}')"
+  interface="$1"
+  private="$(ip -o -4 addr list ${interface} | awk '{print $4}' | cut -d/ -f1)"
   echo -e "  $private"
 }
 
-updates() {
-  updates="$(apt-get -q -y --ignore-hold --allow-change-held-packages --allow-unauthenticated -s dist-upgrade \
+check_updates_apt() {
+  available_updates_count="$(apt-get -q -y --ignore-hold --allow-change-held-packages --allow-unauthenticated -s dist-upgrade \
     | grep  ^Inst | grep -i security | wc -l)"
-  echo -e " $updates"
+  echo -e " $available_updates_count"
+}
+
+check_updates_pacman() {
+  # https://wiki.archlinux.org/title/System_maintenance
+  available_updates_count="$(checkupdates | wc -l)"
+  echo -e " $available_updates_count"
 }
 
 dateTime() {
@@ -20,20 +26,10 @@ dateTime() {
 }
 
 disk() {
-  device="/dev/sda3"
+  #device="/dev/sda2"
+  device="$1"
   usage=$(df -h 2> /dev/null | grep "$device" | awk '{print $3 "/" $2}')
   echo -e " $usage"
-}
-
-weather() {
-  POSTAL_CODE='10115'
-  WEATHER_DEG="$(curl -fGsS --compressed "wttr.in/${POSTAL_CODE}?0q" | grep -Eo "[+,-][0-9]{1,2}" | awk '{print substr($1,2);}')"
-  echo -e " $WEATHER_DEG°C"
-}
-
-bitcoin() {
-  btc=$(curl -sS --compressed https://api.coinbase.com/v2/prices/spot?currency=USD | jq '.data.amount' | sed s/\"//g | cut -d. -f1)
-  echo -e " $btc"
 }
 
 up() {
@@ -56,4 +52,4 @@ battery() {
   fi
 }
 
-xsetroot -name "$(disk) | $(ip) | $(updates) | $(up) | $(battery) | $(dateTime)"
+xsetroot -name "$(disk '/dev/sda2') | $(local_ip 'wlp4s0') | $(up) | $(check_updates_pacman) | $(dateTime)"
