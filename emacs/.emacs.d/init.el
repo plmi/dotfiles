35c62a298ee6383b265d7a5c1929c4e004fc331b
plmi <65455617+plmi@users.noreply.github.com>
2023-10-16 00:42:52 +0200

    Add emacs

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,22 @@
+; disable splash screen
+(setq inhibit-startup-screen t)
+; disable menu bar
+(menu-bar-mode 0)
+; disable toolbar
+(tool-bar-mode 0)
+; set auto-saving directory
+(setq backup-directory-alist '(("." . "/tmp/.emacs_saves")))
+; configure interactive DO things
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+; use system clipboard
+(xclip-mode 1)
+; move custom data out of init.el
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file))
+; add MELPA repository
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
