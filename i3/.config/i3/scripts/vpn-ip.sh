ceff2aacfbf0f34955ce9635e3a36a6b1f2a7f4c
plmi <65455617+plmi@users.noreply.github.com>
2026-03-18 14:39:41 +0100

    Update i3 statusbar VPN script

--- i3/.config/i3/scripts/vpn-ip.sh
+++ i3/.config/i3/scripts/vpn-ip.sh
@@ -1,16 +1,7 @@
 #!/usr/bin/env sh
 
-VPN_NAME="$(nmcli -t -f NAME,TYPE connection show --active | awk -F: '$2=="vpn"{print $1; exit}')"
+VPN_IP="$(ip -4 -o addr show dev tun0 2>/dev/null | awk '{print $4}' | cut -d/ -f1 | head -n1)"
 
-[ -z "$VPN_NAME" ] && exit 0
+[ -z "$VPN_IP" ] && exit 0
 
-# Find tunnel interface
-VPN_IFACE="$(nmcli -t -f DEVICE,TYPE connection show --active | awk -F: '$2=="tun"{print $1; exit}')"
-
-# Fallback if nmcli didn't provide interface
-[ -z "$VPN_IFACE" ] && VPN_IFACE="$(ip -o link | awk -F': ' '/tun|tap/ {print $2; exit}')"
-
-VPN_IP="$(ip -4 -o addr show dev "$VPN_IFACE" 2>/dev/null \
-    | awk '{print $4}' | cut -d/ -f1 | head -n1)"
-
-echo "VPN ${VPN_IP} ($VPN_NAME)"
+echo "VPN ${VPN_IP}"
